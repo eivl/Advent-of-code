ff7a3ee19a594331ed4441dfad19e78c47b00f14
Eivind Teig <eivind.teig@gmail.com>
2018-12-05 11:04:58 +0100

    some multiprocessing testing.

--- day5.py
+++ day5.py
@@ -1,6 +1,7 @@
 from day5_input import DAY5_INPUT
 import string
-
+from timeit import default_timer as timer
+start = timer()
 
 def part1():
     stack = []
@@ -11,16 +12,23 @@ def part1():
             stack.pop()
         else:
             stack.append(char)
-    return len(stack)
-print(part1())
+    return len(stack), stack
+result, stack = part1()
 
-def part2():
+
+end = timer()
+print(result)
+print(end - start)
+start = timer()
+
+
+def part2(DAY5_INPUT):
     shortest_poly = None
     for test_polymer in string.ascii_lowercase:
-        stack = []
+
         modified_input = DAY5_INPUT.replace(test_polymer.upper(), '')
         modified_input = modified_input.replace(test_polymer, '')
-
+        stack = []
         for char in modified_input:
             if len(stack) == 0:
                 stack.append(char)
@@ -28,12 +36,40 @@ def part2():
                 stack.pop()
             else:
                 stack.append(char)
-        if shortest_poly is None or shortest_poly > len(stack):
-            shortest_poly = len(stack)
-            # print(shortest_poly, test_polymer)
-        else:
-            # print('->', len(stack), test_polymer)
-            pass
+        stack_length = len(stack)
+        if shortest_poly is None or shortest_poly > stack_length:
+            shortest_poly = stack_length
     return shortest_poly
 
-print(part2())
+result = part2(DAY5_INPUT)
+end = timer()
+print(result)
+print(end - start)
+
+
+def part2_redone(test_polymer, DAY5_INPUT=DAY5_INPUT):
+    shortest_poly = None
+    modified_input = DAY5_INPUT.replace(test_polymer.upper(), '')
+    modified_input = modified_input.replace(test_polymer, '')
+    stack = []
+    for char in modified_input:
+        if len(stack) == 0:
+            stack.append(char)
+        elif stack[-1] != char and stack[-1].lower() == char.lower():
+            stack.pop()
+        else:
+            stack.append(char)
+    stack_length = len(stack)
+    if shortest_poly is None or shortest_poly > stack_length:
+        shortest_poly = stack_length
+    return shortest_poly
+
+from multiprocessing import Pool
+pool = Pool()
+start = timer()
+result = pool.map(part2_redone, string.ascii_lowercase)
+end = timer()
+pool.close()
+pool.join()
+print(min(result))
+print(end - start)

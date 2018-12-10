647722fb604a2ec694ca221838ad1643e62b4e49
Eivind Teig <eivind.teig@gmail.com>
2018-12-10 13:30:36 +0100

    working version, Sedsarqs print idea

--- day10.py
+++ day10.py
@@ -1,93 +1,63 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from math import inf
-from typing import List
-from scipy.optimize import minimize
-import numpy as np
 import re
 
 with open('day10_input.txt') as f:
     content = f.readlines()
 
-xdata = [int(x[10:16]) for x in content]
-ydata = [int(y[18:24]) for y in content]
-dxdata = [int(dx[36:38]) for dx in content]
-dydata = [int(dy[39:42]) for dy in content]
-
-
-@dataclass(unsafe_hash=True)
-class TwoDPoint:
-    x : int
-    y : int
-    dy: int
-    dx: int
-
-    def __iter__(self):
-        return iter([self.x, self.y, self.dx, self.dy])
-
-    def current_possition(self, time):
-        return (self.x + self.dx*time, self.y + self.dy*time)
-
 @dataclass
-class MyPlot:
-    points: List[TwoDPoint]
-    min_x: float = inf
-    min_y: float = inf
-    max_x: float = -inf
-    max_y: float = -inf
-    index : int = 0
+class MyPoint:
+    pos_x: int
+    pos_y: int
+    vel_x: int
+    vel_y: int
+
+    def step(self):
+        self.pos_x += self.vel_x
+        self.pos_y += self.vel_y
+
+HEIGHT = 10
+drawing_set = set()
+points = []
+
+for line in content:
+    (pos_x, pos_y, vel_x, vel_y) = re.findall(r"-?\d+", line)
+    points.append(MyPoint(int(pos_x), int(pos_y), int(vel_x), int(vel_y)))
+    drawing_set.add((int(pos_x), int(pos_y), int(vel_x), int(vel_y)))
 
 
-    def __iter__(self):
-        return self.points
+def box_dimension(time):
+    min_x, max_x, min_y, max_y = inf, -inf, inf, -inf
+    for x, y, vx, vy in drawing_set:
+        new_x, new_y = x + time*vx, y + time*vy
+        max_x, max_y = max(max_x, new_x), max(max_y, new_y)
+        min_x, min_y = min(min_x, new_x), min(min_y, new_y)
+    width = max_x - min_x + 1
+    height = max_y - min_y + 1
+    return width, height, min_x, min_y
 
-    def __next__(self):
-        try:
-            result = self.points[self.index]
-        except IndexError:
-            raise StopIteration
-        self.index += 1
-        return result
+seconds = 0
 
-    def add_point(self, x, y, dx, dy):
-        self.points.append(TwoDPoint(x, y, dx, dy))
+while True:
+    y = []
 
-    def box_dimension(self, time):
-        min_x, max_x, min_y, max_y = inf, -inf, inf, -inf
-        for x, y, vx, vy in self.points:
-            newx, newy = x + time*vx, y + time*vy
-            min_x = min(min_x, newx)
-            max_x = max(max_x, newx)
-            min_y = min(min_y, newy)
-            max_y = max(max_y, newy)
+    for point in points:
+        point.step()
+        y.append(point.pos_y)
 
-        width = max_x - min_x + 1
-        height = max_y - min_y + 1
-        width, height = float(width), float(height)
-        print(width, height, time)
-        return width, height, float(min_x), float(min_y)
+    seconds += 1
 
-    def box_area(self, time):
-        width, height, *rest = self.box_dimension(time)
-        return width * height
+    if max(y) - min(y) <= HEIGHT:
+        break
 
+width, height, min_x, min_y = box_dimension(seconds)
+text = [[' '] * width for _ in range(height)]
+for pos_x, pos_y, vel_x, vel_y in drawing_set:
+    i = pos_x + seconds * vel_x - min_x
+    j = pos_y + seconds * vel_y - min_y
+    text[j][i] = '#'
 
-my_points = []
-for i, _ in enumerate(xdata):
-     my_points.append(TwoDPoint(xdata[i], ydata[i], dxdata[i], dydata[i]))
+for line in text:
+    print(''.join(line))
+print(seconds)
 
-my_plot = MyPlot(set(my_points))
-
-
-result = minimize(my_plot.box_area, 1)
-seconds = int(round(result.x[0]))
-width, height, min_x, min_y = my_plot.box_dimension(seconds)
-
-# text = [['.'] * width for _ in range(height)]
-# for x, y, vx, vy in points:
-#     i = x + seconds * vx - min_x
-#     j = y + seconds * vy - min_y
-#     text[j][i] = '#'
-
-# for line in text:
-#     print(''.join(line))
-# print(seconds)

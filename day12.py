55cf0f58ca17e8912b1a1320138f3a1ce889a898
Eivind Teig <eivind.teig@gmail.com>
2018-12-12 13:39:56 +0100

    cleaned up a bit

--- day12.py
+++ day12.py
@@ -27,27 +27,11 @@ for line in content:
     else:
         print('Something strange going on: ', line)
 
-backup = binary_state[:]
-
-
-for gen in range(generations):
-    bstate_copy = binary_state[:]
-    for i, b in enumerate(binary_state):
-        if binary_state[i:RULE_LENGHT+i] in grow_rules:
-            bstate_copy[i+2] = 1
-        else:
-            try:
-                bstate_copy[i+2] = 0
-            except IndexError:
-                pass
-    binary_state = bstate_copy[:]
-    if sum(binary_state[-5:]) > 0:
-        binary_state.insert(len(binary_state), 0)
-    if gen == 19:
-        print(sum([i if x else 0 for i, x in enumerate(binary_state, start=-5)]))
-
 
 def calculate_generation(array, gen):
+    '''
+    generates a binary array based on the grow_rules
+    '''
     for gen in range(gen):
         bin_state_copy = array[:]
         for i, b in enumerate(array):
@@ -63,26 +47,34 @@ def calculate_generation(array, gen):
             array.insert(len(array), 0)
     return array
 
+
 def sum_binary_array(array):
+    '''
+    sums up a binary array that has 5 zeroes as padding
+    '''
     return sum([i if x else 0 for i, x in enumerate(array, start=-5)])
 
 
-def get_first_index(array):
+def get_indexs(array):
+    '''
+    returns a list of indexes for the elements in the array
+    used to manually find a stable increasing generation as a baseline
+    '''
     index_list = []
     for i, n in enumerate(array):
         if n:
             index_list.append(i)
     return index_list
 
-my_sum = sum(get_first_index(calculate_generation(backup, 130))) -(20*5)
 
+print(sum_binary_array(calculate_generation(binary_state, 20)))
 
 crazy_gen = 50_000_000_000
 # at generation 130 the plants have stabilized and now you just have to offset the indexes.. a programatic solution is to come later.
 baseline = 130
-indexes = [100, 105, 111, 118, 123, 128, 134, 142, 150, 158, 163, 168, 173, 181, 189, 198, 208, 213, 219, 227]
+stable_indexes = [100, 105, 111, 118, 123, 128, 134, 142, 150, 158, 163, 168, 173, 181, 189, 198, 208, 213, 219, 227]
 first_attempt = []
-for i in indexes:
+for i in stable_indexes:
     first_attempt.append(i+(crazy_gen-baseline))
 
 print(sum(first_attempt)-100)

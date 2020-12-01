686744c1cd15381fdd98fd26ada804a1aab069ca
Eivind Teig <eivind.teig@gmail.com>
2020-12-01 07:54:59 +0100

    Day 1

--- day1.py
+++ day1.py
@@ -1,15 +1,17 @@
+from itertools import combinations
+
+
 with open('day1_input.txt') as f:
     result = f.readlines()
 result = [int(line.strip()) for line in result]
 
-sum_part1 = sum((num//3)-2 for num in result)
-print(sum_part1)
+for num_a, num_b in combinations(result, 2):
+    if num_a + num_b == 2020:
+        print(num_a*num_b)
+        break
 
 
-def ff(f):
-    if (f//3)-2 < 0:
-        return 0
-    return f//3-2 + ff(f//3-2)
-
-sum_part2 = sum(ff(num) for num in result)
-print(sum_part2)
+for num_a, num_b, num_c in combinations(result, 3):
+    if num_a + num_b + num_c == 2020:
+        print(num_a*num_b*num_c)
+        break

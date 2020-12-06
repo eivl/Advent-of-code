5ba498f90d44160e8f8dc33b92f66518a6f92e60
Eivind Teig <eivind.teig@gmail.com>
2020-12-06 09:07:58 +0100

    Day 6

---
+++ day6.py
@@ -0,0 +1,10 @@
+with open('day6_input.txt') as f:
+    result = f.read()
+
+result = result.split('\n\n')
+
+print(sum([len(set(''.join(ans.split()))) for ans in result]))
+
+data = [[set(ans) for ans in chunk.split()] for chunk in result]
+print(sum([len(ans) for ans in map(lambda a: set.intersection(*a), data)]))
+
--- user1.py
+++ user1.py
@@ -1,6 +1,47 @@
-x = True
-y = False
-condition =  1 in [1, 2, 3]
+import math
 
-print(x if condition else y)
-print((y, x)[condition])
+file = open("day4_input.txt")
+
+ids = []
+
+min = 0
+max = 127
+row = 0
+
+ID = 0
+
+minS = 0
+maxS = 7
+seat = 0
+
+for line in file:
+    for letter in line[0:7:1]:
+        if letter == "F":
+            max = math.ceil(max - ((max - min)/2))
+        else:
+            min = math.ceil(max - ((max - min)/2))
+    if letter == "F":
+        row = min
+    else:
+        row = max
+    min = 0
+    max = 127
+
+    for letter in line[7:]:
+        if letter == "L":
+            maxS = math.ceil(maxS - ((maxS - minS)/2))
+        else:
+            minS = math.ceil(maxS - ((maxS - minS)/2))
+    if letter == "L":
+        seat = minS
+    else:
+        seat = maxS
+    minS = 0
+    maxS = 7
+
+    ID = (row*8) + seat
+
+    print ("Row: " + str(row) + " Col: " + str(seat) + " ID: " + str(ID))
+    ids.append(ID)
+ids.sort()
+print(len(ids))

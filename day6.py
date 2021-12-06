bce5656209989ad22e72489db37cea1437b4f20c
Eivind Teig <eivind.teig@gmail.com>
2021-12-06 08:50:55 +0100

    Add measure

--- day6.py
+++ day6.py
@@ -1,3 +1,20 @@
+from functools import wraps
+from time import perf_counter as time
+
+
+def measure(func):
+    @wraps(func)
+    def _time_it(*args, **kwargs):
+        start = time()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end_ = time() - start
+            print(f"Exe-time of {func.__name__}: {end_ if end_ > 0 else 0} "
+                  f"sec")
+    return _time_it
+
+
 with open('day6_input.txt') as f:
     result, *_ = f.read().splitlines()
 
@@ -32,7 +49,7 @@ fish = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 for spawncycle in [int(num) for num in result.split(',')]:
     fish[spawncycle] += 1
 
-
+@measure
 def number_of_fish(n):
     fishes = fish.copy()
     for i in range(n):

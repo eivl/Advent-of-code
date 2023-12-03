cd4b31de7e1735f13986553791b4df6c308f524f
Eivind Teig <eivind.teig@gmail.com>
2023-12-03 19:21:50 +0100

    Solution B not working

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 example = '''\
 1abc2
 pqr3stu8vwx
@@ -21,3 +23,55 @@ with open('day1_input.txt') as file:
 
 result = calibration_value(content)
 print(sum(result))
+
+# Part 2
+example = '''\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+'''
+
+NUMBER_MAPPING = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
+def fix_calibration_string(string):
+    '''
+    eightwothree -> 8wo3
+    '''
+    result = []
+    for line in string.splitlines():
+        replace_mapping = {}
+
+        for key in NUMBER_MAPPING:
+            if key in line:
+                replace_mapping[key] = line.find(key)
+
+        sorted_mapping = sorted(replace_mapping.items(), key=itemgetter(1))
+
+        for key, value in sorted_mapping:
+            line = line.replace(key, NUMBER_MAPPING[key])
+
+        result.append(line)
+    string = '\n'.join(result)
+    return string
+
+
+assert fix_calibration_string('eightwothree') == '8wo3'
+real_example = fix_calibration_string(example)
+assert sum(calibration_value(real_example)) == 281
+
+real_input = fix_calibration_string(content)
+print(sum(calibration_value(real_input)))
